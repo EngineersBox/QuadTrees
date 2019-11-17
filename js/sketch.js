2080649a476762c5bd4281018626de6132f26c46
EngineersBox <toastcraft.info@gmail.com>
2019-11-17 18:11:34 +1100

    Fixed icon and added boundary capacity configurable value

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,6 @@
 let C_WIDTH;
 let C_HEIGHT;
+let BOUNDARY_CAPACITY
 let POINT_COUNT;
 
 let canvas;
@@ -8,6 +9,7 @@ let quad;
 function initConst() {
     C_WIDTH = parseFloat(Utils.getElem("c_width").value);
     C_HEIGHT = parseFloat(Utils.getElem("c_height").value);
+    BOUNDARY_CAPACITY = parseInt(Utils.getElem("boundary_capacity").value);
     POINT_COUNT = parseInt(Utils.getElem("point_count").value);
 }
 
@@ -16,7 +18,7 @@ function setup() {
     canvas = createCanvas(C_WIDTH, C_HEIGHT);
     canvas.parent("sketch_view");
 
-    quad = new QuadTree(1, new AABB(createVector(C_WIDTH / 2, C_HEIGHT / 2), C_WIDTH / 2));
+    quad = new QuadTree(BOUNDARY_CAPACITY, new AABB(createVector(C_WIDTH / 2, C_HEIGHT / 2), C_WIDTH / 2));
 
     for (let i = 0; i < POINT_COUNT; i++) {
         quad.insert(createVector(Math.random() * C_WIDTH, Math.random() * C_HEIGHT));
